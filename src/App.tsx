0d626cd66020304dee6f005f6f427b98ea0a0506
skip-rat <skip-rat@users.noreply.github.com>
2023-02-07 13:34:46 +0000

    Initial Commit
    Completed all exercises  except #6. Watched all video resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,39 @@
 
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/header';
 import CharacterContainer from './components/character_container';
 import Navigation from './components/navigation';
 import { DisneyCharacter } from './disney_character';
+import axios from 'axios';
+
+export const FavouritesContext = React.createContext<number[]>([]);
 
 const App : React.FC = () => {
 
 	const [currentPage, setCurrentPage] = useState<number>(1);
-
-  // Some dummy state representing disney characters
-  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
-    {
-      _id: 6,
-      name: "'Olu Mel",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
-    },
-    {
-      _id: 25,
-      name: "Abu",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Abu.png"
-    },
-    {
-      _id: 30,
-      name: "Ace",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
-    },
-  ]);
-
+  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
+  const [characterFavourites, setCharacterFavourites] = useState<Array<number>>([]);
   
+  const getCharacters = async (pageNumber : number) => {
+    // Utilised Axios for API calls
+    const apiResponse = await axios.get(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
+    setCharacters(apiResponse.data.data);
+  };
 
-  return (
-    <div className="page">
-      <Header currentPage={currentPage} />
-      <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <CharacterContainer characters={characters} />
-    </div>
-  );
+  useEffect(() => {
+    getCharacters(currentPage);
+  }, [currentPage]);
+
+	return (
+		<FavouritesContext.Provider value={characterFavourites}>
+		<div className="page">
+			<Header currentPage={currentPage} />
+			<Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
+			<CharacterContainer characters={characters} updateFavourites={setCharacterFavourites}  />
+		</div>
+		</FavouritesContext.Provider>
+	);
 }
 
 export default App;
--- src/components/character.tsx
+++ src/components/character.tsx
@@ -1,21 +1,40 @@
 import { DisneyCharacter } from "../disney_character"
+import React, { useContext } from 'react';
+import { FavouritesContext } from '../App';
 
-// for our props we can reuse the DisneyCharacter interface
-// - defining an anonymous type that just has one property - a DisneyCharacter
-const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => 
-  
-    <article className="character-item">
+interface CharacterProps{
+	character: DisneyCharacter;
+	updateFavourites: (favourites: Array<number>) => void;
+}
+
+const Character : React.FC<CharacterProps> = ( { character, updateFavourites }) => {
+
+	const characterFavourites = useContext(FavouritesContext);
+
+	function toggleFavouriteForCharacter(characterId : number) {
+		if(!characterFavourites.includes(characterId)) {
+			// add to favourites
+			updateFavourites([...characterFavourites, characterId]);
+		}
+		else {
+		  // remove from favourites
+		  const updatedFavourites = characterFavourites.filter((id) => id !== characterId);
+		  updateFavourites(updatedFavourites);
+		}
+	  }
+
+    return(<article className="character-item">
 
       <h2>{character.name}</h2>
 
-      <div className="character-item__actions">
-        Add to Favourites
+      <div className="character-item__actions"  onClick={() => toggleFavouriteForCharacter(character._id)}>
+	  	{!characterFavourites.includes(character._id) ? "Add to Favourites" : "Favourited"}
       </div>
 
       <img className="character-item__img" src={character.imageUrl} alt={character.name} />
 
-    </article>
-  
+    </article>);
+}
 
 
-export default Character
+export default Character;
